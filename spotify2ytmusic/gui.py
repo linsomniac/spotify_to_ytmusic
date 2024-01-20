94d9faf99e181cee258391d874017e1dbe8fe7d6
Yoween <larochetristan17@gmail.com>
2024-01-20 14:56:32 +0100

    Code reformatting

--- spotify2ytmusic/gui.py
+++ spotify2ytmusic/gui.py
@@ -1,30 +1,24 @@
+import os
+import subprocess
+import sys
+import threading
 import tkinter as tk
 from tkinter import ttk
+
+import cli
+import spotify_backup
 from reverse_playlist import reverse_playlist
-import spotify_backup, cli
-import sys, os, logging, threading, subprocess
 
-class LoggingToGUI(logging.Handler):
-    """ Used to redirect logging output to the widget passed in parameters """
-    def __init__(self, console):
-        logging.Handler.__init__(self)
-        self.console = console
 
-    def emit(self, message): 
-        formattedMessage = self.format(message)
-        self.console.configure(state=tk.NORMAL)
-        self.console.insert(tk.END, formattedMessage)
-        self.console.configure(state=tk.DISABLED)
-        self.console.see(tk.END)
-        print(message)
-        
 def create_label(parent, text, **kwargs):
     return tk.Label(parent, text=text, font=("Helvetica", 14), **kwargs)
+
 
 def create_button(parent, text, **kwargs):
     return tk.Button(parent, text=text, font=("Helvetica", 14), **kwargs)
 
-class Window():
+
+class Window:
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title("Spotify to YT Music")
@@ -32,7 +26,7 @@ class Window():
 
         # Redirect stdout to GUI
         sys.stdout.write = self.redirector
-        
+
         self.root.after(1, lambda: self.yt_login(auto=True))
 
         # Create a PanedWindow with vertical orientation
@@ -58,7 +52,7 @@ class Window():
         self.tab4 = ttk.Frame(self.tabControl)
         self.tab5 = ttk.Frame(self.tabControl)
         self.tab6 = ttk.Frame(self.tabControl)
-        
+
         self.tabControl.add(self.tab0, text='Login to YT Music')
         self.tabControl.add(self.tab1, text='Spotify backup')
         self.tabControl.add(self.tab2, text='Reverse playlist')
@@ -74,34 +68,48 @@ class Window():
         # Create the Text widget for the logs
         self.logs = tk.Text(self.log_frame, font=("Helvetica", 14))
         self.logs.pack(fill=tk.BOTH, expand=1)
-        
+
         # tab 0
-        create_label(self.tab0, text="Welcome to Spotify to YT Music!\nTo start, you need to login to YT Music.").pack(anchor=tk.CENTER)
+        create_label(self.tab0, text="Welcome to Spotify to YT Music!\nTo start, you need to login to YT Music.").pack(
+            anchor=tk.CENTER)
         create_button(self.tab0, text="Login", command=self.yt_login).pack(anchor=tk.CENTER)
 
         # tab1
         create_label(self.tab1, text="First, you need to backup your spotify playlists").pack(anchor=tk.CENTER)
-        create_button(self.tab1, text="Backup", command=lambda: self.call_func(spotify_backup.main, self.tab2)).pack(anchor=tk.CENTER)
+        create_button(self.tab1, text="Backup", command=lambda: self.call_func(spotify_backup.main, self.tab2)).pack(
+            anchor=tk.CENTER)
 
         # tab2
-        create_label(self.tab2, text="Since this program likes the last added song first, you need to reverse the playlist if you want to keep the exact same playlists.\nBut this step is not mandatory, you can skip it if you don't mind by clicking here.").pack(anchor=tk.CENTER)
+        create_label(self.tab2,
+                     text="Since this program likes the last added song first, you need to reverse the playlist if "
+                          "you want to keep the exact same playlists.\nBut this step is not mandatory, you can skip "
+                          "it if you don't mind by clicking here.").pack(
+            anchor=tk.CENTER)
         create_button(self.tab2, text="Skip", command=lambda: self.tabControl.select(self.tab3)).pack(anchor=tk.CENTER)
         create_button(self.tab2, text="Reverse", command=self.call_reverse).pack(anchor=tk.CENTER)
 
         # tab3
         create_label(self.tab3, text="Now, you can load your liked songs.").pack(anchor=tk.CENTER)
-        create_button(self.tab3, text="Load", command=lambda: self.call_func(cli.load_liked, self.tab4)).pack(anchor=tk.CENTER)
-        
+        create_button(self.tab3, text="Load", command=lambda: self.call_func(cli.load_liked, self.tab4)).pack(
+            anchor=tk.CENTER)
+
         # tab4
-        create_label(self.tab4, text="Here, you can get a list of your playlists, with their ID.").pack(anchor=tk.CENTER)
-        create_button(self.tab4, text="List", command=lambda: self.call_func(cli.list_playlists, self.tab5)).pack(anchor=tk.CENTER)
+        create_label(self.tab4, text="Here, you can get a list of your playlists, with their ID.").pack(
+            anchor=tk.CENTER)
+        create_button(self.tab4, text="List", command=lambda: self.call_func(cli.list_playlists, self.tab5)).pack(
+            anchor=tk.CENTER)
 
         # tab5
-        create_label(self.tab5, text="Here, you can copy all your playlists from Spotify to YT Music. Please note that this step can take a long time since songs are added one by one.").pack(anchor=tk.CENTER)
-        create_button(self.tab5, text="Copy", command=lambda: self.call_func(cli.copy_all_playlists, self.tab6)).pack(anchor=tk.CENTER)
-        
+        create_label(self.tab5,
+                     text="Here, you can copy all your playlists from Spotify to YT Music. Please note that this step "
+                          "can take a long time since songs are added one by one.").pack(
+            anchor=tk.CENTER)
+        create_button(self.tab5, text="Copy", command=lambda: self.call_func(cli.copy_all_playlists, self.tab6)).pack(
+            anchor=tk.CENTER)
+
         # tab6
-        create_label(self.tab6, text="Here, you can copy a specific playlist from Spotify to YT Music.").pack(anchor=tk.CENTER)
+        create_label(self.tab6, text="Here, you can copy a specific playlist from Spotify to YT Music.").pack(
+            anchor=tk.CENTER)
         create_label(self.tab6, text="Spotify playlist ID:").pack(anchor=tk.CENTER)
         self.spotify_playlist_id = tk.Entry(self.tab6)
         self.spotify_playlist_id.pack(anchor=tk.CENTER)
@@ -110,13 +118,12 @@ class Window():
         self.yt_playlist_id.pack(anchor=tk.CENTER)
         create_button(self.tab6, text="Copy", command=self.call_copy_playlist).pack(anchor=tk.CENTER)
 
-
-    def redirector(self, inputStr):
+    def redirector(self, input_str):
         self.logs.config(state=tk.NORMAL)
-        self.logs.insert(tk.INSERT, inputStr)
+        self.logs.insert(tk.INSERT, input_str)
         self.logs.config(state=tk.DISABLED)
         self.logs.see(tk.END)
-        
+
     def call_func(self, func, next_tab):
         th = threading.Thread(target=func)
         th.start()
@@ -124,21 +131,22 @@ class Window():
             self.root.update()
         self.tabControl.select(next_tab)
         print()
-        
+
     def call_copy_playlist(self):
         if self.spotify_playlist_id.get() == "" or self.yt_playlist_id.get() == "":
             print("Please enter both playlist IDs")
             return
-        th = threading.Thread(target=cli.copy_playlist, args=(self.spotify_playlist_id.get(), self.yt_playlist_id.get()))
+        th = threading.Thread(target=cli.copy_playlist,
+                              args=(self.spotify_playlist_id.get(), self.yt_playlist_id.get()))
         th.start()
         while th.is_alive():
             self.root.update()
-            
+
         self.tabControl.select(self.tab6)
         print()
 
     def call_reverse(self):
-        result = [None]  # Shared data structure
+        result = [0]  # Shared data structure
 
         def target():
             result[0] = reverse_playlist(replace=True)  # Call the function with specific arguments
@@ -151,7 +159,6 @@ class Window():
         if result[0] == 0:  # Access the return value
             self.tabControl.select(self.tab3)
             print()
-
 
     def yt_login(self, auto=False):
         def run_in_thread():
@@ -171,16 +178,16 @@ class Window():
                     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
                 process.communicate()
-                
+
                 print("Login successful")
-                
+
             self.tabControl.select(self.tab1)
             print()
-
 
         # Run the function in a separate thread
         th = threading.Thread(target=run_in_thread)
         th.start()
+
 
 if __name__ == "__main__":
     ui = Window()
--- spotify2ytmusic/reverse_playlist.py
+++ spotify2ytmusic/reverse_playlist.py
@@ -1,13 +1,16 @@
-import json, os, shutil
+import json
+import os
+import shutil
 from argparse import ArgumentParser
 
-def reverse_playlist(input_file = "playlists.json", verbose = True, replace = False) -> None:
+
+def reverse_playlist(input_file="playlists.json", verbose=True, replace=False) -> int:
     if os.path.exists(input_file) and not replace:
         if verbose:
             print("Output file already exists and no replace argument detected, exiting...")
         return 1
 
-    print("Backuping file...")
+    print("Backing up file...")
     shutil.copyfile(input_file, input_file.split(".")[0] + "_backup.json")
     # Load the JSON file
     with open(input_file, "r") as file:
@@ -30,19 +33,20 @@ def reverse_playlist(input_file = "playlists.json", verbose = True, replace = Fa
     # Write the modified JSON back to the file
     with open(input_file, "w") as file:
         json.dump(data2, file)
-    
+
     if verbose:
         print("Done!")
         print(f"File can be found at {input_file}")
-    
+
     return 0
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("input_file", type=str, help="Path to the input file")    
+    parser.add_argument("input_file", type=str, help="Path to the input file")
     parser.add_argument("-v", "--verbose", action="store_false", help="Enable verbose mode")
     parser.add_argument("-r", "--replace", action="store_true", help="Replace the output file if already existing")
 
     args = parser.parse_args()
-    
+
     reverse_playlist(args.input_file, args.verbose, args.replace)
