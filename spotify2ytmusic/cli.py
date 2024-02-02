bc51ee17fd669cecdba1c4224c54012f8d94aa77
Sean Reifschneider <sean@realgo.com>
2024-02-01 20:44:57 -0700

    Starting work on integrating GUI

--- spotify2ytmusic/cli.py
+++ spotify2ytmusic/cli.py
@@ -7,80 +7,9 @@ import time
 from argparse import ArgumentParser
 from ytmusicapi import YTMusic
 from typing import Optional, Union, Iterator
-from collections import namedtuple
 
-
-SongInfo = namedtuple("SongInfo", ["title", "artist", "album"])
-
-
-def iter_spotify_liked_albums(
-    spotify_playlist_file: str = "playlists.json",
-    spotify_encoding: str = "utf-8",
-) -> Iterator[SongInfo]:
-    """Songs from liked albums on Spotify."""
-    spotify_pls = load_playlists_json(spotify_playlist_file, spotify_encoding)
-
-    if not "albums" in spotify_pls:
-        return None
-
-    for album in [x["album"] for x in spotify_pls["albums"]]:
-        for track in album["tracks"]["items"]:
-            yield SongInfo(track["name"], track["artists"][0]["name"], album["name"])
-
-
-def iter_spotify_playlist(
-    src_pl_id: Optional[str] = None,
-    spotify_playlist_file: str = "playlists.json",
-    spotify_encoding: str = "utf-8",
-) -> Iterator[SongInfo]:
-    """Songs from a specific album ("Liked Songs" if None)"""
-    spotify_pls = load_playlists_json(spotify_playlist_file, spotify_encoding)
-
-    for src_pl in spotify_pls["playlists"]:
-        if src_pl_id is None:
-            if str(src_pl.get("name")) != "Liked Songs":
-                continue
-        else:
-            if str(src_pl.get("id")) != src_pl_id:
-                continue
-
-        src_pl_name = src_pl["name"]
-
-        print(f"== Spotify Playlist: {src_pl_name}")
-
-        for src_track in reversed(src_pl["tracks"]):
-            if src_track["track"] is None:
-                print(
-                    f"WARNING: Spotify track seems to be malformed, Skipping.  Track: {src_track!r}"
-                )
-                continue
-
-            try:
-                src_album_name = src_track["track"]["album"]["name"]
-                src_track_artist = src_track["track"]["artists"][0]["name"]
-            except TypeError as e:
-                print(
-                    f"ERROR: Spotify track seems to be malformed.  Track: {src_track!r}"
-                )
-                raise (e)
-            src_track_name = src_track["track"]["name"]
-
-            yield SongInfo(src_track_name, src_track_artist, src_album_name)
-
-
-def get_ytmusic() -> YTMusic:
-    if not os.path.exists("oauth.json"):
-        print("ERROR: No file 'oauth.json' exists in the current directory.")
-        print("       Have you logged in to YTMusic?  Run 'ytmusicapi oauth' to login")
-        sys.exit(1)
-
-    try:
-        return YTMusic("oauth.json")
-    except json.decoder.JSONDecodeError as e:
-        print(f"ERROR: JSON Decode error while trying start YTMusic: {e}")
-        print("       This typically means a problem with a 'oauth.json' file.")
-        print("       Have you logged in to YTMusic?  Run 'ytmusicapi oauth' to login")
-        sys.exit(1)
+from . import backend
+from .backend import SongInfo, get_ytmusic
 
 
 def get_playlist_id_by_name(yt: YTMusic, title: str) -> Optional[str]:
@@ -96,7 +25,7 @@ def list_liked_albums():
     """
     List albums that have been liked.
     """
-    for song in iter_spotify_liked_albums():
+    for song in backend.iter_spotify_liked_albums():
         print(f"{song.album} - {song.artist} - {song.title}")
 
 
@@ -121,36 +50,6 @@ def list_playlists():
         print(f"{pl['playlistId']} - {pl['title']:40} ({pl.get('count', '?')} tracks)")
 
 
-def _ytmusic_create_playlist(yt: YTMusic, title: str, description: str) -> str:
-    def _create(yt: YTMusic, title: str, description: str) -> Union[str, dict]:
-        exception_sleep = 5
-        for _ in range(10):
-            try:
-                """Create a playlist on YTMusic, retrying if it fails."""
-                id = yt.create_playlist(title=title, description=description)
-                return id
-            except Exception as e:
-                print(
-                    f"ERROR: (Retrying create_playlist: {title}) {e} in {exception_sleep} seconds"
-                )
-                time.sleep(exception_sleep)
-                exception_sleep *= 2
-
-        return {
-            "s2yt error": 'ERROR: Could not create playlist "{title}" after multiple retries'
-        }
-
-    id = _create(yt, title, description)
-    #  create_playlist returns a dict if there was an error
-    if isinstance(id, dict):
-        print(f"ERROR: Failed to create playlist (name: {title}): {id}")
-        sys.exit(1)
-
-    time.sleep(1)  # seems to be needed to avoid missing playlist ID error
-
-    return id
-
-
 def create_playlist():
     """
     Create a YTMusic playlist
@@ -160,11 +59,7 @@ def create_playlist():
         sys.exit(1)
 
     pl_name = sys.argv[1]
-
-    yt = get_ytmusic()
-
-    id = _ytmusic_create_playlist(yt, title=pl_name, description=pl_name)
-    print(f"Playlist ID: {id}")
+    backend.create_playlist(pl_name)
 
 
 def lookup_song(yt, track_name, artist_name, album_name):
@@ -271,7 +166,9 @@ def load_liked_albums():
     spotify_pls = load_playlists_json()
 
     copier(
-        iter_spotify_liked_albums(spotify_encoding=args.spotify_playlists_encoding),
+        backend.iter_spotify_liked_albums(
+            spotify_encoding=args.spotify_playlists_encoding
+        ),
         None,
         args.dry_run,
         args.track_sleep,
@@ -307,7 +204,9 @@ def load_liked():
     args = parse_arguments()
 
     copier(
-        iter_spotify_playlist(None, spotify_encoding=args.spotify_playlists_encoding),
+        backend.iter_spotify_playlist(
+            None, spotify_encoding=args.spotify_playlists_encoding
+        ),
         None,
         args.dry_run,
         args.track_sleep,
@@ -371,7 +270,7 @@ def copy_playlist():
             print(f"NOTE: Created playlist '{pl_name}' with ID: {dst_pl_id}")
 
     copier(
-        iter_spotify_playlist(
+        backend.iter_spotify_playlist(
             src_pl_id, spotify_encoding=args.spotify_playlists_encoding
         ),
         dst_pl_id,
@@ -432,7 +331,7 @@ def copy_all_playlists():
             print(f"NOTE: Created playlist '{pl_name}' with ID: {dst_pl_id}")
 
         copier(
-            iter_spotify_playlist(
+            backend.iter_spotify_playlist(
                 src_pl["id"], spotify_encoding=args.spotify_playlists_encoding
             ),
             dst_pl_id,
